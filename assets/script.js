90d9fe68d46d8118d5af50a8ddfd1031211af50a
Abdul Nur <abduljabar@Abduls-MacBook-Pro.local>
2022-11-26 10:31:23 -0600

    i removed the place holder text on top of the page, fixed the am/pm error, added some comments to javascript,html, and css. fixed save button not aligning with text area. and made everything a little bigger.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var today = document.querySelector("#currentDay")
-var blueButtom = document.querySelectorAll(".saveBtn")   
+var blueButtom = document.querySelectorAll(".saveBtn")  
+var container = document.querySelector(".container")
 var textArea = document.getElementById("textarea")
 var textArea1 = document.getElementById("textarea1")
 var textArea2 = document.getElementById("textarea2")
@@ -25,75 +26,71 @@ var currentTime = dayjs().format(' H');
 
 var num = Number(currentTime)
 console.log(num);
-//collum one
+//block one
 var timeNum = Number(time)
+// the two variables above changes the string into a number.
 console.log(timeNum);
  if (num === timeNum){
-    lol.textContent="lol"
+    
     textArea.classList.add('present');
 } else if(num < timeNum ){
     textArea.classList.add('future')
 }
-//collum two
+//block two
 var timeNum1 = Number(time1)
  if (num === timeNum1 ){
-    lol.textContent="lol"
+    
     textArea1.classList.add('present');
 } else if(num < timeNum1){
     textArea1.classList.add('future')
 } 
-//collum three
+//block three
 var timeNum2 = Number(time2)
  if (num === timeNum2 ){
-    lol.textContent="lol"
+    
     textArea2.classList.add('present');
 } else if(num < timeNum2){
     textArea2.classList.add('future')
 } 
-//collum four
+//block four
 var timeNum3 = Number(time3)
  if (num === timeNum3 ){
-    lol.textContent="lol"
+    
     textArea3.classList.add('present');
 } else if(num < timeNum3){
     textArea3.classList.add('future')
 } 
-//collum five
+//block five
 var timeNum4 = Number(time4)
- if (num === timeNum4 ){
-    lol.textContent="lol"
+ if (num === timeNum4 +12){
     textArea4.classList.add('present');
-} else if(num < timeNum4){
+} else if(num < timeNum4 +12){
     textArea4.classList.add('future')
 } 
-//collum six
+//block six
 var timeNum5 = Number(time5)
- if (num === timeNum5 ){
-    lol.textContent="lol"
+ if (num === timeNum5 +12 ){
     textArea5.classList.add('present');
-} else if(num < timeNum5){
+} else if(num < timeNum5 +12){
     textArea5.classList.add('future')
 } 
-//collum seven
+//block seven
 var timeNum6 = Number(time6)
  if (num === timeNum6 +12 ){
-    lol.textContent="lol"
     textArea6.classList.add('present');
-} else if(num < timeNum6){
+} else if(num < timeNum6 +12 ){
     textArea6.classList.add('future')
 } 
-//collum eight
+//block eight
 var timeNum7 = Number(time7)
  if (num === timeNum7 + 12){
-    lol.textContent="lol"
     textArea7.classList.add('present');
-} else if(num < timeNum7){
+} else if(num < timeNum7 +12){
     textArea7.classList.add('future')
 } 
-//collum nine
+//block nine
 var timeNum8 = Number(time8)
  if (num === timeNum8 +12 ){
-    lol.textContent="lol"
     textArea8.classList.add('present');
 } else if(num < timeNum8 + 12){
     textArea8.classList.add('future')
@@ -118,11 +115,11 @@ textArea8.textContent = localStorage.getItem("checknow8")
     
       }, 1000);
 
-//textArea.innerHTML="hi"
- blueButtom.forEach(event => {
-event.textContent = "no"
 
-     event.addEventListener("click",function(){
+ //blueButtom.forEach(event => {
+//event.textContent = "no"
+
+container.addEventListener("click",function(){
     
     
     var input = document.getElementById('textarea').value;
@@ -157,4 +154,4 @@ localStorage.setItem("checknow8",input8)
     console.log(input);
  });
     
- })
+ //})
